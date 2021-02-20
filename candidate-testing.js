b4f3388b309e1be74dbdf95076a058159e0cd349
bhossa <bhossa@gmail.com>
2021-02-20 12:55:22 +0000

    Last version passed, trying original array with enter instead of space after each array entry

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,22 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", 
+let questions = [ 
+"Who was the first American woman in space? ",
+"True or false: 5000 meters == 5 kilometers? ",
+"(5+3)/2*10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
+
+
+
+
+
+/*["Who was the first American woman in space? ", 
                 "True or false: 5 kilometer == 5000 meters? ", 
                 "(5 + 3)/2 * 10 = ? ", 
                 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
-                "What is the minimum crew size for the ISS? "];
+                "What is the minimum crew size for the ISS? "];*/
 
 
 
@@ -34,8 +45,8 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 for (let i = 0; i < questions.length; i++) {
-  /*candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}`);*/
-  candidateAnswers[i] = input.question(`${questions[i]}`);
+  candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}`);
+  
   console.log(`Your Answer: ${candidateAnswers[i]} \nCorrect Answer: ${correctAnswers[i]}`);
   console.log("\n");
 }
